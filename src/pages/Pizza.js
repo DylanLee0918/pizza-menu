b3e9b42e6262ef5c3c59a1838c7571798739a886
Dylan Lee <leedylan0918@gmail.com>
2025-11-05 21:36:31 +0800

    Added styles in pizza menu

--- src/pages/Pizza.js
+++ src/pages/Pizza.js
@@ -4,17 +4,27 @@ const Pizza = (props) => {
   return (
     <div>
       {props.data.map((pizza) => (
-        <div key={pizza.name} style={{ opacity: pizza.soldOut ? 0.5 : 1 }}>
-          <h3>
-            {pizza.name} {pizza.soldOut && "(Sold Out)"}
-          </h3>
-          <p>{pizza.ingredients}</p>
-          <p>Price: ${pizza.price}</p>
+        <div
+          className="pizza"
+          key={pizza.name}
+          style={{ opacity: pizza.soldOut ? 0.5 : 1 }}
+        >
           <img
             src={pizza.photoName}
             alt={pizza.name}
             style={{ width: "200px" }}
           />
+          <div>
+            <h3 style={{ fontSize: "1.7em", marginBottom: "1.4em" }}>
+              {pizza.name} {pizza.soldOut && "(Sold Out)"}
+            </h3>
+            <p style={{ fontSize: "1.4em", marginBottom: "0.5em" }}>
+              {pizza.ingredients}
+            </p>
+            <p style={{ fontSize: "1.4em", marginBottom: "1em" }}>
+              Price: ${pizza.price}
+            </p>
+          </div>
         </div>
       ))}
     </div>
